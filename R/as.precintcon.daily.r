2ae80f0683fb923c406f434cbce5be080b34a1db
Anna Ukkola <annaukkola@rsess-mbp.ccrc.unsw.edu.au>
2019-12-16 11:44:39 +1100

    removing date check, formatted differently for daily

--- R/as.precintcon.daily.r
+++ R/as.precintcon.daily.r
@@ -2,16 +2,15 @@
 as.precintcon.daily <- function(object, na.value = NA) {
 
 	if (!is.element("data.frame", class(object)) || ncol(object) != 3)
-		stop("Invalid object. It must be a \"data.frame\" and contain 33 numeric columns.")
+		stop("Invalid object. It must be a \"data.frame\" and contain 3 numeric columns.")
 	
-	for (i in 3) {
-		for (j in 1:nrow(object)) {
-			if (is.na(as.Date(paste(object[j,1], object[j,2], i - 2, sep = "-"), "%Y-%m-%d"))) {
-				a <- NA
-				object[j,i] <- a  
-			}
-		}
-	}
+		# for (j in 1:nrow(object)) {
+		# 	if (is.na(as.Date(paste(object[j,1], object[j,2], sep = "-"), "%Y-%d"))) {
+		# 		a <- NA
+		# 		object[j,3] <- a  
+		# 	}
+		# }
+
 	
 	colnames(object) <- c("year", "day", paste(rep("d", 1), 1, sep=""))
 	
